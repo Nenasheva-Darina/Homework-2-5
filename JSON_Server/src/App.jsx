0e46a658cb67f0623896008ac0c8d5eb24015094
Nenasheva-Darina <dydykina1997@mail.ru>
2025-04-23 16:24:17 +0400

    Реализовала поиск ToDo

--- JSON_Server/src/App.jsx
+++ JSON_Server/src/App.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useState, useRef } from 'react';
 import styles from './App.module.css';
 import { SortIMG } from './SortIMG';
 import { ToDoList } from './ToDoList/ToDoContainer.jsx';
@@ -9,10 +9,19 @@ export const App = () => {
   const [downloadButtonActive, setDownloadButtonActive] = useState(false); // Кнопка Добавить дело
   const [updateToDoList, setUpdateToDoList] = useState(false); // Флаг кнопки редактирования
   const [refreshToDoFlag, setRefreshToDoFlag] = useState(false); // Флаг обновления списка дел
-  const [inputValue, setInputValue] = useState(''); // Сброс значения инпута
+  const [inputValue, setInputValue] = useState(''); // Значения инпута ввода задачи
   const [errorMessage, setErrorMessage] = useState(''); // Хранилище ошибок
   const [isChecked, setIsChecked] = useState(false); //  Состояние для галочки
   const [alphabetically, setAlphabetically] = useState(false);
+  const [searchFlag, setSearchFlag] = useState(false); // Флаг кнопки поиска
+  const [searchInputValue, setSearchInputValue] = useState(''); // Значения инпута ввода поиска
+
+  const searchInputRef = useRef(null);
+  useEffect(() => {
+    if (searchFlag) {
+      searchInputRef.current.focus();
+    }
+  }, [searchFlag]);
 
   const refreshToDo = () => setRefreshToDoFlag(!refreshToDoFlag);
 
@@ -27,27 +36,30 @@ export const App = () => {
   }, [refreshToDoFlag]);
 
   const requestAddNewToDo = () => {
-    setDownloadButtonActive(true);
-    setIsLoadingLoader(true);
-
-    fetch('http://localhost:3004/toDoList', {
-      method: 'POST',
-      headers: { 'Content-Type': 'application/json;charset=utf-8' },
-      body: JSON.stringify({
-        title: inputValue,
-      }),
-    })
-      .then((rawResponse) => rawResponse.json())
-      .then((response) => {
-        console.log('Добавленно новое дело:', response);
-        setToDoList([...toDoList, response]);
-        // console.log(response);
-        setRefreshToDoFlag(!refreshToDoFlag);
-        setInputValue('');
-        refreshToDo();
+    if (inputValue === '') {
+      setErrorMessage('Упс...Кажется вы забыли написать задачу');
+    } else {
+      setDownloadButtonActive(true);
+      setIsLoadingLoader(true);
+      fetch('http://localhost:3004/toDoList', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json;charset=utf-8' },
+        body: JSON.stringify({
+          title: inputValue,
+        }),
       })
-      .catch((error) => console.log('Ошибочка...', error))
-      .finally(() => setUpdateToDoList(false));
+        .then((rawResponse) => rawResponse.json())
+        .then((response) => {
+          console.log('Добавленно новое дело:', response);
+          setToDoList([...toDoList, response]);
+          // console.log(response);
+          setRefreshToDoFlag(!refreshToDoFlag);
+          setInputValue('');
+          refreshToDo();
+        })
+        .catch((error) => console.log('Ошибочка...', error))
+        .finally(() => setUpdateToDoList(false));
+    }
   };
 
   const handleChange = (target) => {
@@ -84,6 +96,31 @@ export const App = () => {
     return newfilterArr;
   };
 
+  const searchClasses = searchFlag ? styles.completed : null;
+
+  const newFilterArrToDo = toDoList.filter((task) => {
+    const title = task.title.toLowerCase();
+    const difficultTask = searchInputValue.toLowerCase();
+
+    return title.includes(difficultTask);
+  });
+  console.log(newFilterArrToDo);
+
+  const searchToDoList = () => {
+    setSearchFlag(!searchFlag);
+  };
+  //   console.log(searchInputValue);
+
+  const penChangeSearch = (target) => {
+    if (target.value === '') {
+      setErrorMessage('Упс...Кажется вы забыли написать задачу');
+      setSearchInputValue('');
+    } else {
+      setSearchInputValue(target.value);
+      setErrorMessage('');
+    }
+  };
+
   return (
     <>
       <div className={styles.boxToDo}>
@@ -94,19 +131,39 @@ export const App = () => {
         <h1> Мой список дел </h1>
 
         <div className={styles.boxToDo1Div}>
-          <input
-            type="text"
-            placeholder="Какое приключение на сегодня?"
-            className={styles.newToDoInput}
-            value={inputValue}
-            onChange={(event) => handleChange(event.target)}
-            onKeyDown={(event) => {
-              if (event.key === 'Enter') {
-                requestAddNewToDo();
-              }
-            }}
+          <img
+            className={searchClasses}
+            src="/search.png"
+            alt="Perform a search"
+            onClick={searchToDoList}
           />
+          {searchFlag ? (
+            <input
+              type="text"
+              placeholder="Что ищем?"
+              className={styles.newToDoInput}
+              value={searchInputValue}
+              ref={searchInputRef}
+              onChange={(event) => penChangeSearch(event.target)}
+            />
+          ) : (
+            <input
+              type="text"
+              placeholder="Какое приключение на сегодня?"
+              className={styles.newToDoInput}
+              value={inputValue}
+              onChange={(event) => handleChange(event.target)}
+              onKeyDown={(event) => {
+                if (event.key === 'Enter') {
+                  requestAddNewToDo();
+                }
+              }}
+            />
+          )}
           <SortIMG setAlphabetically={setAlphabetically} />
+          <button className={styles.button} onClick={requestAddNewToDo}>
+            Добавить
+          </button>
         </div>
 
         <div className={styles.content}>
@@ -114,6 +171,26 @@ export const App = () => {
 
           {alphabetically
             ? sorterFilterArr().map(({ id, title, completed }) => (
+                <div key={id} className={styles.toDoList}>
+                  <input
+                    className={styles.boxCheck}
+                    type="checkbox"
+                    checked={completed}
+                    onClick={() => handleCheckboxChange(id)}
+                  />
+
+                  <ToDoList
+                    key={id}
+                    id={id}
+                    title={title}
+                    completed={completed}
+                    taskUpdate={handleUpdateTitle}
+                    refreshToDo={refreshToDo}
+                  />
+                </div>
+              ))
+            : searchFlag
+            ? newFilterArrToDo.map(({ id, title, completed }) => (
                 <div key={id} className={styles.toDoList}>
                   <input
                     className={styles.boxCheck}
@@ -152,10 +229,6 @@ export const App = () => {
                 </div>
               ))}
         </div>
-
-        <button className={styles.button} onClick={requestAddNewToDo}>
-          Добавить новое дело
-        </button>
       </div>
     </>
   );
